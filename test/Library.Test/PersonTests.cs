e93d15ddbaa6e8b8932e8f0bc2e33d5337867265
Sebastian Fripp <sebastian.fripp@correo.ucu.edu.uy>
2021-09-09 23:11:22 -0300

    Agregado de settings y tests

--- test/Library.Test/PersonTests.cs
+++ test/Library.Test/PersonTests.cs
@@ -4,18 +4,58 @@ using UnitTestAndDebug;
 
 namespace Tests
 {
+    [TestFixture]
     public class Tests
     {
-        [SetUp]
-        public void Setup()
+        [Test]
+        public void IdInvalida() // Cambiá el nombre para indicar qué estás probando
         {
-            // Insertá tu código de inicialización aquí
+            // Configuracion del estado
+            string expectedName = "Sebastian Fripp";
+            string expectedId = null;
+            string testId = "4.989.466-6";
+            string testName = "Sebastian Fripp";
+
+            // Prueba el comportamiento que quiero probar
+            Person person = new Person(testName, testId);
+
+            // Comprobacion del resultado
+            Assert.AreEqual(expectedName, person.Name);
+            Assert.AreEqual(expectedId, person.ID);
         }
 
         [Test]
-        public void Test1() // Cambiá el nombre para indicar qué estás probando
+        public void NombreInvalido()
         {
-            // Insertá tu código  de pruebaaquí
+            // Configuracion del estado
+            string expectedName = null;
+            string expectedId = "4.989.466-8";
+            string testId = "4.989.466-8";
+            string testName = "";
+
+            // Prueba el comportamiento que quiero probar
+            Person person = new Person(testName, testId);
+
+            // Comprobacion del resultado
+            Assert.AreEqual(expectedName, person.Name);
+            Assert.AreEqual(expectedId, person.ID);
+        }
+
+        [Test]
+        public void DatosInvalidos()
+        {
+            // Configuracion del estado
+            string expectedName = null;
+            string expectedId = null;
+            string testId = "4.989.466-6";
+            string testName = "";
+
+            // Prueba el comportamiento que quiero probar
+            Person person = new Person(testName, testId);
+
+            // Comprobacion del resultado
+            Assert.AreEqual(expectedName, person.Name);
+            Assert.AreEqual(expectedId, person.ID);
         }
     }
 }
